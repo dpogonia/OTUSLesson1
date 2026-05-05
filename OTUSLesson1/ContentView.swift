4250ad40adb55d78de7f022b63cb454bf1d4d48b
Dmitrii Pogonia <dpogonia@yahoo.com>
2026-05-05 10:30:34 +0300

    Initial Commit

---
+++ OTUSLesson1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OTUSLesson1
+//
+//  Created by Dmitrii Pogonia on 05.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OTUSLesson1/OTUSLesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSLesson1App.swift
+//  OTUSLesson1
+//
+//  Created by Dmitrii Pogonia on 05.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct OTUSLesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
